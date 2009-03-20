b2199396118508eabc61ae694a1da9711056d421
Tim Diggins <tim@red56.co.uk>
2009-03-20 01:30:43 +0000

    modified cap task and README for cap 2 (Advanced Rails Recipes mods) not yet tested!

--- recipes/maintenance_with_permatime.rb
+++ recipes/maintenance_with_permatime.rb
@@ -1,23 +1,22 @@
 #needs permatime maintenance recipe for cap
 #thanks to Mike Clark for helpful pointers at http://clarkware.com/cgi/blosxom/2007/01/05 and in Advanced Rails Recipes
 
-task :disable_web, :roles => :web do
-  on_rollback { delete "#{shared_path}/system/maintenance.html" }
-  
-  maintenance = render("app/views/layouts/maintenance.html.erb", 
-      :deadline => ENV['UNTIL'],
-                       :reason => ENV['REASON'])
-                       
-  put maintenance, "#{shared_path}/system/maintenance.html", 
-      :mode => 0644
-end
-
-def render(path_to_template_file,  options)
-  path_to_template_file=File.dirname(__FILE__) + "/../../../"+path_to_template_file
-  `ls -al #{path_to_template_file}`
-  deadline = options[:deadline]
-  reason = options[:reason]
-  require 'erb'
-  template = File.read(path_to_template_file)
-  result = ERB.new(template).result(binding)
+namespace :deploy do
+  namespace :web do 
+    desc "serve up a custom maintenance page with useful permatime of when went offline"
+    task :disable, :roles => :web do
+      require 'erb'
+      
+      on_rollback { run "rm #{shared_path}/system/maintenance.html" }
+      
+      path_to_template_file=File.dirname(__FILE__) + "/../../../app/views/layouts/maintenance.html.erb"
+      
+      deadline = ENV['UNTIL']
+      reason = ENV['REASON']
+      template = File.read(path_to_template_file)
+      page = ERB.new(template).result(binding)
+      
+      put page, "#{shared_path}/system/maintenance.html", :mode => 0644
+    end
+  end 
 end
