29057868f6fb5ec33353e1f3e3f0c2ce47850c47
Tim Diggins <tim@red56.co.uk>
2009-03-20 09:25:57 +0000

    improved output once again

--- install.rb
+++ install.rb
@@ -3,17 +3,24 @@ require 'ftools'
  
 maintenance_layout = File.expand_path(RAILS_ROOT)+"/app/views/layouts/maintenance.html.erb"
 default_maintenance_layout = File.expand_path(File.dirname(__FILE__))+"/maintenance.html.erb"
-puts "-" *80
+
+def hr
+  puts "-" *80
+
 if File::exists? maintenance_layout
+  hr
   puts "You already have a maintenance layout at #{maintenance_layout}."
+  hr
   puts "But, you might want to compare with ours:"
   puts "  #{default_maintenance_layout}"
+  hr
 else  
+  hr
   puts "Installing default maintenance layout"
   puts "  to #{maintenance_layout}"
+  hr
   puts "You definitely want to customize this" 
   puts "  (put your correct APP_NAME and email address)"
   File.copy default_maintenance_layout, maintenance_layout
-puts "-" *80
-
+  hr
 end
