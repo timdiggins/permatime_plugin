fbf0a538755df6b1c1af3221b81bcac11d6ec0e9
Tim Diggins <tim@red56.co.uk>
2009-03-20 09:23:30 +0000

    improved info in comment in maintenance file and output of install hook

--- install.rb
+++ install.rb
@@ -3,15 +3,17 @@ require 'ftools'
  
 maintenance_layout = File.expand_path(RAILS_ROOT)+"/app/views/layouts/maintenance.html.erb"
 default_maintenance_layout = File.expand_path(File.dirname(__FILE__))+"/maintenance.html.erb"
-  puts "  from #{default_maintenance_layout}"
-  puts "  to   #{maintenance_layout}"
+puts "-" *80
 if File::exists? maintenance_layout
   puts "You already have a maintenance layout at #{maintenance_layout}."
   puts "But, you might want to compare with ours:"
   puts "  #{default_maintenance_layout}"
 else  
   puts "Installing default maintenance layout"
-  puts "  From #{default_maintenance_layout}"
-  puts "You may want to customize this"
+  puts "  to #{maintenance_layout}"
+  puts "You definitely want to customize this" 
+  puts "  (put your correct APP_NAME and email address)"
   File.copy default_maintenance_layout, maintenance_layout
+puts "-" *80
+
 end
